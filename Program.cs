c2e84cd7e6df424e09e8caa60a408c684df85211
Razmik Minasyan <razmik.minasyann@gmail.com>
2021-09-21 21:30:36 +0400

    "Fname and Lname"

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname,lname;
+            Console.Write("Enter your first name : ");
+            fname = Console.ReadLine();
+            Console.Write("Enter your last name : ");
+            lname = Console.ReadLine();
+            Console.Write("Your full name is "+fname+" "+lname);
+        }
+    }
+}
